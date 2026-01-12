6f674ef824131288734075f55b761214a997680a
yeonzeop <ksp2617@naver.com>
2026-01-12 13:38:22 +0900

    init: spring boot project 생성

---
+++ src/test/java/com/example/off/OffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.off;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
