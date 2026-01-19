1cd88d028b70b0d347533c633722994027533b95
yeonzeop <ksp2617@naver.com>
2026-01-19 14:54:58 +0900

    [Feat] #2: Project Class

---
+++ src/main/java/com/example/off/domain/project/Project.java
@@ -0,0 +1,35 @@
+package com.example.off.domain.project;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "project_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String requirement;
+
+    @Column(nullable = false)
+    private Long estimation;
+
+    @Column(nullable = false)
+    private LocalDate start;
+
+    @Column(nullable = false)
+    private LocalDate end;
+}
