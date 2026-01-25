59da90b819183ac8e6c48ddb487898aad87962d4
yeonzeop <ksp2617@naver.com>
2026-01-25 17:02:23 +0900

    [Feat] #11: NotificationType에 String 필드 추가

--- src/main/java/com/example/off/domain/notification/NotificationType.java
+++ src/main/java/com/example/off/domain/notification/NotificationType.java
@@ -1,7 +1,13 @@
 package com.example.off.domain.notification;
 
 public enum NotificationType {
-    INVITE,
-    CHAT,
-    PAY;
+    INVITE("파트너 매칭"),
+    CHAT("채팅"),
+    PAY("결제");
+
+    private final String description;
+
+    NotificationType(String description) {
+        this.description = description;
+    }
 }
