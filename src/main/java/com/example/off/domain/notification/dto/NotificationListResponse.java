27335981e66faf2064f8272116d079cca9b85b42
yeonzeop <ksp2617@naver.com>
2026-01-25 20:16:57 +0900

    [Chore] #11: yml 설정 및 Getter 어노테이션

--- src/main/java/com/example/off/domain/notification/dto/NotificationListResponse.java
+++ src/main/java/com/example/off/domain/notification/dto/NotificationListResponse.java
@@ -1,9 +1,11 @@
 package com.example.off.domain.notification.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.util.List;
 
+@Getter
 @AllArgsConstructor
 public class NotificationListResponse {
     private int unReadCount;
