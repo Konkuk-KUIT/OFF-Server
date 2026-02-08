0b91dc6faa9748473982dbf6f10400964031218b
G4hyun <sally_0113@naver.com>
2026-02-08 18:41:09 +0900

    [Fix] #22: repository 메서드 삭제

--- src/main/java/com/example/off/domain/projectMember/repository/ProjectMemberRepository.java
+++ src/main/java/com/example/off/domain/projectMember/repository/ProjectMemberRepository.java
@@ -11,14 +11,6 @@ import java.util.Optional;
 public interface ProjectMemberRepository extends JpaRepository<ProjectMember, Long> {
     List<ProjectMember> findAllByMember_Id(Long memberId);
     @Query("""
-            select case when count(pm) > 0 then true else false and
-            from ProjectMember pm
-            where pm.member_id = :memberId
-                and pm.project.endDate > :now
-            """)
-    boolean existsWorkingProject(Long memberId, LocalDateTime now);
-
-    @Query("""
     select pm
     from ProjectMember pm
     where pm.member.id = :memberId
